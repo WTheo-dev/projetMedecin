b8901bf3c4c5bf1097f2c1077aa9947b092dba45
Theo <wazydrag.theo@gmail.com>
2023-11-17 14:29:36 +0100

    modifBDD

--- bdd_data.sql
+++ bdd_data.sql
@@ -1,26 +1,26 @@
--- Insertion de données de test pour la table medecin
-INSERT INTO `medecin` (`Civilité`, `Nom`, `Prénom`) VALUES
-('M.', 'Smith', 'John'),
-('Mme', 'Johnson', 'Emily'),
-('M.', 'Lee', 'David'),
-('Mme', 'Wilson', 'Sarah'),
-('M.', 'Brown', 'Michael'),
-('Mme', 'Smith', 'Emma');
+    -- Insertion de données de test pour la table medecin
+    INSERT INTO `medecin` (`Civilité`, `Nom`, `Prénom`, `id_utilisateur`) VALUES
+    ('M.', 'Smith', 'John'),
+    ('Mme', 'Johnson', 'Emily'),
+    ('M.', 'Lee', 'David'),
+    ('Mme', 'Wilson', 'Sarah'),
+    ('M.', 'Brown', 'Michael'),
+    ('Mme', 'Smith', 'Emma');
 
--- Insertion de données de test pour la table patient
-INSERT INTO `patient` (`NumSecu`, `Civilité`, `nom`, `Prénom`, `Adresse`, `DateNaissance`, `LieuNaissance`) VALUES
-('1234567890', 'Mme', 'Dupont', 'Sophie', '123 Rue de la Santé', '1980-05-15', 'Paris'),
-('9876543210', 'M.', 'Martin', 'Luc', '456 Avenue des Lilas', '1995-12-10', 'Marseille'),
-('5555555555', 'Mme', 'Garcia', 'Elena', '789 Chemin du Soleil', '1972-07-20', 'Lyon'),
-('1111111111', 'M.', 'Tremblay', 'Pierre', '321 Avenue des Roses', '1990-03-25', 'Montréal'),
-('2222222222', 'Mme', 'Andersen', 'Anna', '555 Elm Street', '1985-08-02', 'Copenhague'),
-('3333333333', 'M.', 'Li', 'Jian', '123 Bamboo Lane', '1978-11-12', 'Shanghai');
+    -- Insertion de données de test pour la table patient
+    INSERT INTO `patient` (`NumSecu`, `Civilité`, `nom`, `Prénom`, `Adresse`, `DateNaissance`, `LieuNaissance`) VALUES
+    ('1234567890', 'Mme', 'Dupont', 'Sophie', '123 Rue de la Santé', '1980-05-15', 'Paris'),
+    ('9876543210', 'M.', 'Martin', 'Luc', '456 Avenue des Lilas', '1995-12-10', 'Marseille'),
+    ('5555555555', 'Mme', 'Garcia', 'Elena', '789 Chemin du Soleil', '1972-07-20', 'Lyon'),
+    ('1111111111', 'M.', 'Tremblay', 'Pierre', '321 Avenue des Roses', '1990-03-25', 'Montréal'),
+    ('2222222222', 'Mme', 'Andersen', 'Anna', '555 Elm Street', '1985-08-02', 'Copenhague'),
+    ('3333333333', 'M.', 'Li', 'Jian', '123 Bamboo Lane', '1978-11-12', 'Shanghai');
 
--- Insertion de données de test pour la table rendezvous
-INSERT INTO `rendezvous` (`Id_patient`, `DateHeureRDV`, `DureeRDV`, `id_medecin`) VALUES
-(1, '2023-10-10 09:00:00', '00:30:00', 1),
-(2, '2023-10-15 14:30:00', '00:45:00', 3),
-(3, '2023-10-20 11:15:00', '01:00:00', 2),
-(4, '2023-10-25 10:30:00', '00:30:00', 4),
-(5, '2023-11-05 15:15:00', '00:45:00', 6),
-(6, '2023-11-10 08:45:00', '01:15:00', 5);
+    -- Insertion de données de test pour la table rendezvous
+    INSERT INTO `rendezvous` (`Id_patient`, `DateHeureRDV`, `DureeRDV`, `id_medecin`) VALUES
+    (1, '2023-10-10 09:00:00', '00:30:00', 1),
+    (2, '2023-10-15 14:30:00', '00:45:00', 3),
+    (3, '2023-10-20 11:15:00', '01:00:00', 2),
+    (4, '2023-10-25 10:30:00', '00:30:00', 4),
+    (5, '2023-11-05 15:15:00', '00:45:00', 6),
+    (6, '2023-11-10 08:45:00', '01:15:00', 5);
--- bdd_table.sql
+++ bdd_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 06 oct. 2023 à 10:14
+-- Généré le : ven. 17 nov. 2023 à 11:20
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -29,12 +29,14 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `medecin`;
 CREATE TABLE IF NOT EXISTS `medecin` (
-  `id_medecin` int NOT NULL AUTO_INCREMENT,
-  `Civilité` char(10) DEFAULT NULL,
-  `Nom` varchar(50) DEFAULT NULL,
-  `Prénom` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_medecin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_medecin` int NOT NULL,
+  `Civilité` char(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Nom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Prénom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Id_utilisateur` int NOT NULL,
+  PRIMARY KEY (`id_medecin`),
+  UNIQUE KEY `Id_utilisateur` (`Id_utilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,16 +46,16 @@ CREATE TABLE IF NOT EXISTS `medecin` (
 
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
-  `Id_patient` int NOT NULL AUTO_INCREMENT,
-  `NumSecu` char(50) NOT NULL,
-  `Civilité` char(10) DEFAULT NULL,
-  `nom` varchar(50) DEFAULT NULL,
-  `Prénom` varchar(50) DEFAULT NULL,
-  `Adresse` varchar(50) DEFAULT NULL,
+  `id_patient` int NOT NULL AUTO_INCREMENT,
+  `NumSecu` char(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Civilité` char(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Prénom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Adresse` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `DateNaissance` date DEFAULT NULL,
-  `LieuNaissance` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Id_patient`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `LieuNaissance` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id_patient`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -63,13 +65,43 @@ CREATE TABLE IF NOT EXISTS `patient` (
 
 DROP TABLE IF EXISTS `rendezvous`;
 CREATE TABLE IF NOT EXISTS `rendezvous` (
-  `Id_patient` int NOT NULL,
-  `DateHeureRDV` datetime NOT NULL,
+  `id_patient` int NOT NULL,
+  `id_Rendezvous` int NOT NULL,
+  `dateHeureRDV` datetime NOT NULL,
   `DureeRDV` time DEFAULT NULL,
   `id_medecin` int NOT NULL,
-  PRIMARY KEY (`Id_patient`,`DateHeureRDV`),
+  PRIMARY KEY (`id_patient`,`id_Rendezvous`),
   KEY `id_medecin` (`id_medecin`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rôle`
+--
+
+DROP TABLE IF EXISTS `rôle`;
+CREATE TABLE IF NOT EXISTS `rôle` (
+  `Id_Rôle` int NOT NULL AUTO_INCREMENT,
+  `Description` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`Id_Rôle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `Id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nom_utilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mdp` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Id_Rôle` int NOT NULL,
+  PRIMARY KEY (`Id_utilisateur`),
+  KEY `Id_Rôle` (`Id_Rôle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
