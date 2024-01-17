a68f0c3b4e781883c956031429ecfb2b752794b7
Twisten <47831964+Twisten@users.noreply.github.com>
2024-01-17 15:39:37 +0100

    modif connexion

--- connexion.php
+++ connexion.php
@@ -4,63 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion Médecin</title>
-    <style>
-        .body-connexion {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            flex-direction: column; /* Permet au contenu d'être empilé verticalement */
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .h1-connexion {
-            margin-top: 0;
-            margin-bottom: 20px;
-        }
-
-        .form-connexion {
-            background-color: #fff;
-            border-radius: 8px;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 300px;
-            text-align: center;
-        }
-
-        .label-connexion {
-            display: block;
-            margin-bottom: 8px;
-            font-weight: bold;
-        }
-
-        .input-connexion {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 12px;
-            box-sizing: border-box;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        .connexion-button {
-            background-color: #4caf50;
-            color: #fff;
-            padding: 10px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        .connexion-button:hover {
-            background-color: #45a049;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body class="body-connexion">
 
     <h1 class="h1-connexion">Se connecter</h1>
