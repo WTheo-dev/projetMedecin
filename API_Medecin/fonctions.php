37826cda00b5ba1f74e25eebde1a2e1fb710e054
Theo <wazydrag.theo@gmail.com>
2024-01-18 12:45:24 +0100

    connexionOK

--- API_Medecin/fonctions.php
+++ API_Medecin/fonctions.php
@@ -19,22 +19,6 @@ function connexionBD()
     return $BD;
 }
 
-function identification($nom_utilisateur, $mdp)
-{
-    $BD = connexionBD();
-    $nom_utilisateur = htmlspecialchars($nom_utilisateur);
-    $mdp = htmlspecialchars($mdp);
-
-    $verificationMembre = $BD->prepare('SELECT * FROM utilisateur WHERE nom_utilisateur = ? AND mdp = ?');
-    $verificationMembre->execute(array($nom_utilisateur, $mdp));
-    $BD = null;
-    if ($verificationMembre->rowCount() > 0) {
-        return TRUE;
-    } else {
-        return FALSE;
-    }
-}
-
 function recuperation_role($nom_utilisateur)
 {
     $BD = connexionBD();
--- connexion.php
+++ connexion.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+include_once('server.php');
+
+// Si le formulaire de connexion est soumis
+if (isset($_POST['nom_utilisateur']) && isset($_POST['mdp'])) {
+    // Connexion et création du token
+    connexion($_POST['nom_utilisateur'], $_POST['mdp']);
+} else {
+    // Affiche un message d'erreur si les champs ne sont pas définis
+    echo "Veuillez remplir tous les champs du formulaire.";
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -23,64 +36,5 @@
 
 </body>
 </html>
-<?php
-session_start();
-include_once('./API_Medecin/fonctions.php');
-
-// Si le formulaire de connexion est soumis
-if (isset($_POST['nom_utilisateur']) && isset($_POST['mdp'])) {
-    // Construit les données à envoyer à l'API
-    $data = array(
-        'nom_utilisateur' => $_POST['nom_utilisateur'],
-        'mdp' => $_POST['mdp']
-    );
-
-    // Convertit les données en format JSON
-    $json_data = json_encode($data);
-
-    // URL de l'API
-    $api_url = 'http://localhost/projetMedecin/API_Medecin/serveurToken.php';
-
-    // Options pour la requête HTTP
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'POST',
-            'content' => $json_data,
-        ),
-    );
-
-    // Crée le contexte pour la requête HTTP
-    $context  = stream_context_create($options);
-
-    // Effectue la requête HTTP et récupère la réponse
-    $response = file_get_contents($api_url, false, $context);
-
-    // Vérifie si la réponse est réussie
-    if ($response !== false) {
-        // Décoder la réponse JSON
-        $token = json_decode($response, true);
-
-        // Vérifie si la création du token a réussi
-        if ($token) {
-            // Stocke le token en session ou faites ce que vous devez faire avec le token
-            $_SESSION['token'] = $token;
-
-            // Redirige l'utilisateur vers la page souhaitée après la connexion
-            header('Location: index.php');
-            exit();
-        } else {
-            // Gestion des erreurs lors de la création du token
-            echo "Erreur lors de la création du token. Veuillez réessayer.";
-        }
-    } else {
-        // Gestion des erreurs lors de la requête HTTP
-        echo "Erreur lors de la communication avec l'API. Veuillez réessayer.";
-    }
-} else {
-    // Affiche un message d'erreur si les champs ne sont pas définis
-    echo "Veuillez remplir tous les champs du formulaire.";
-}
-?>
 
 
